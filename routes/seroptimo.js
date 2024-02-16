a74c30b3811c28f7d7a3b7cf183be0f3b5d312a2
profesorjand <profesorjand@gmail.com>
2024-02-16 18:13:55 -0300

    Corrigiendo errores de env variables

--- routes/seroptimo.js
+++ routes/seroptimo.js
@@ -27,32 +27,32 @@ router.get('/', async (req, res) => {
     doshaInfo: {
       [VATA_ID]: {
         nombre: 'Vata',
-        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${TRIDOSHA_ID}aaa`,
+        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${VATA_ID}aaa`,
         doshaURL: DOSHA_URL_VATA,
       },
       [PITTA_ID]: {
         nombre: 'Pitta',
-        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${TRIDOSHA_ID}`,
+        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${PITTA_ID}`,
         doshaURL: DOSHA_URL_PITTA,
       },
       [KAPHA_ID]: {
         nombre: 'Kapha',
-        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${TRIDOSHA_ID}`,
+        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${KAPHA_ID}`,
         doshaURL: DOSHA_URL_KAPHA,
       },
       [VATA_PITTA_ID]: {
         nombre: 'Vata - Pitta',
-        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${TRIDOSHA_ID}`,
+        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${VATA_PITTA_ID}`,
         doshaURL: DOSHA_URL_VATA_PITTA,
       },
       [VATA_KAPHA_ID]: {
         nombre: 'Vata - Kapha',
-        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${TRIDOSHA_ID}`,
+        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${VATA_KAPHA_ID}`,
         doshaURL: DOSHA_URL_VATA_KAPHA,
       },
       [PITTA_KAPHA_ID]: {
         nombre: 'Pitta - Kapha',
-        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${TRIDOSHA_ID}`,
+        paypalURL: `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${PITTA_KAPHA_ID}`,
         doshaURL: DOSHA_URL_PITTA_KAPHA,
       },
       [TRIDOSHA_ID]: {
